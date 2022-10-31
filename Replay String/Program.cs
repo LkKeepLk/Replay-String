b116ce4a4b7db5172247195f01706c1c9dfe0888
aleks <aleks@DESKTOP-ELH28KS>
2022-10-31 18:39:46 +0300

    Добавьте файлы проекта.

---
+++ Replay String/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+while (true)
+{
+    Console.WriteLine("Введите Строку");
+    string h = Console.ReadLine();
+    //bool noom = int.TryParse(h, out int n);
+    //if (noom = false)
+    //{
+    //    Console.WriteLine("Впишите корректные данные");
+    //    return;
+    //}
+    Console.WriteLine("Введите число сколько раз должна повторяться строка");
+    string j = Console.ReadLine();
+    bool moon = int.TryParse(j, out int m);
+    if (moon = false)
+    {
+        Console.WriteLine("Впишите корректные данные");
+        return;
+    }
+    var str = new StringBuilder();
+    for (int i = 0; i < m; i++)
+    {
+        str.Append(h);
+    }
+    Console.WriteLine(str.ToString());
+}
